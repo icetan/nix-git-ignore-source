37a7d564b5566d0c20a8499bf6a56723b1c4a4c3
Christopher Fredén <c.freden@gmail.com>
2017-10-26 18:11:37 +0200

    Expose filter function

--- default.nix
+++ default.nix
@@ -2,25 +2,27 @@
   match = re: x: builtins.match re x != null;
   nmatch = re: x: ! (match re x);
   globToRegex = x: "^" + (builtins.replaceStrings ["." "*"] ["\\." ".*"] x) + "$";
-  gitIgnoreSource = src: let
-    relPath = path: lib.removePrefix (toString src + "/") (toString path);
-    gitignore = (toString src) + "/.gitignore";
+
+  gitIgnoreFilter = gitignore: path: type: let
+    baseName = baseNameOf (toString path);
+    relPath = lib.removePrefix (dirOf (toString gitignore) + "/") (toString path);
     ignoreGlob = lib.filter
       (nmatch "^\\s*($|#.*)")
       (lib.splitString "\n" (builtins.readFile gitignore));
     ignoreRegex = map globToRegex ignoreGlob;
+  in (
+    (lib.sources.cleanSourceFilter path type) &&
+    (type != "symlink") &&
+    ! (lib.any (re:
+      (match re relPath) ||
+      (match re baseName)
+    ) ignoreRegex)
+  );
+
+  gitIgnoreSource = src: let
+    gitignore = (toString src) + "/.gitignore";
   in
-    builtins.filterSource (path: type: let
-      baseName = baseNameOf (toString path);
-      relPath' = relPath path;
-    in (
-      (lib.sources.cleanSourceFilter path type) &&
-      (type != "symlink") &&
-      ! (lib.any (re:
-           (match re relPath') ||
-           (match re baseName)
-      ) ignoreRegex)
-    )) src;
+    builtins.filterSource (gitIgnoreFilter gitignore) src;
 in {
-  inherit gitIgnoreSource;
+  inherit gitIgnoreSource gitIgnoreFilter;
 }

f4fa2d845424059b3e6de56ea399fb63736a5adc
Christopher Fredén <c.freden@gmail.com>
2017-10-27 17:17:59 +0200

    Clean up of imports and traces

--- default.nix
+++ default.nix
@@ -1,35 +1,42 @@
 { pkgs ? (import <nixpkgs> {}) }: with pkgs; let
-  match = re: x: builtins.match re x != null;
-  nmatch = re: x: ! (match re x);
-  globToRegex = x: "^" + (builtins.replaceStrings ["." "*"] ["\\." "[^/]*"] x) + "$";
-  drop = x: builtins.substring 0 ((lib.stringLength x) - 1) x;
-  lastChar = x: builtins.substring ((lib.stringLength x) - 1) 1 x;
+  inherit (builtins) trace match replaceStrings substring readFile filterSource
+    getEnv;
+  inherit (lib) stringLength filter removePrefix optionalString splitString any
+    sources;
+
+  debug = message: value: if (getEnv "NIX_DEBUG" != "") then (trace message value) else value;
+
+  match' = re: x: match re x != null;
+  nmatch' = re: x: ! (match' re x);
+  globToRegex = x: "^" + (replaceStrings ["." "*"] ["\\." "[^/]*"] x) + "$";
+  drop = x: substring 0 ((stringLength x) - 1) x;
+  lastChar = x: substring ((stringLength x) - 1) 1 x;
   isDir = x: lastChar x == "/";
 
   gitIgnoreFilter = gitignore: path: type: let
     baseName = baseNameOf (toString path);
-    relPath = lib.removePrefix (dirOf (toString gitignore) + "/") (toString path);
-    ignoreGlob = lib.filter
-      (nmatch "^\\s*($|#.*)")
-      (lib.splitString "\n" (builtins.readFile gitignore));
+    relPath = removePrefix (dirOf (toString gitignore) + "/") (toString path);
+    ignoreGlob = filter
+      (nmatch' "^\\s*($|#.*)")
+      (splitString "\n" (readFile gitignore));
     ignoreRegex = map globToRegex ignoreGlob;
   in (
-    (lib.sources.cleanSourceFilter path type) &&
+    (sources.cleanSourceFilter path type) &&
     (type != "symlink") &&
-    ! (lib.any (re: let
-      ifDir = lib.optionalString ( (isDir (drop re)) && (type == "directory") ) "/";
+    ! (any (re: let
+      ifDir = optionalString ( (isDir (drop re)) && (type == "directory") ) "/";
     in
-      ( (match re (relPath + ifDir)) || (match re (baseName + ifDir)) )
+      ( (match' re (relPath + ifDir)) || (match' re (baseName + ifDir)) )
     ) ignoreRegex)
   );
 
   gitIgnoreSource = src: let
     gitignore = (toString src) + "/.gitignore";
   in
-    builtins.filterSource (p: t: let
+    filterSource (p: t: let
       pass = gitIgnoreFilter gitignore p t;
     in
-      builtins.trace "${if pass then "o" else "x"} ${p} ${t}" pass
+      debug "${if pass then "o" else "x"} ${p} ${t}" pass
     ) src;
 in {
   inherit gitIgnoreSource gitIgnoreFilter;

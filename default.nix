c4c4449744e45d9e1ab6dc6e0e5eb9e217d1c68f
Christopher Fredén <me@icetan.org>
2019-01-31 16:41:36 +0100

    Add partial support for ! globs

--- default.nix
+++ default.nix
@@ -2,42 +2,60 @@
   inherit (builtins) trace match replaceStrings substring readFile filterSource
     getEnv;
   inherit (lib) stringLength filter removePrefix optionalString splitString any
-    sources;
+    concatStringsSep;
+  inherit (lib.sources) cleanSourceFilter cleanSourceWith;
 
   debug = message: value: if (getEnv "NIX_DEBUG" != "") then (trace message value) else value;
+  debugMatch = re: value:
+    let matched = match' re value;
+    in debug "regex: ${re} ${if matched then "o" else "x"} ${value}" matched;
+  debugFilter = filter: name: path: type:
+    let pass = filter path type;
+    in debug "filter: ${name} ${if pass then "o" else "x"} ${path} ${type}" pass;
 
   match' = re: x: match re x != null;
   nmatch' = re: x: ! (match' re x);
-  globToRegex = x: "^" + (replaceStrings ["." "*"] ["\\." "[^/]*"] x) + "$";
+  globToRegex = x: ".*/${replaceStrings ["." "**" "*" "?"] ["\\." ".*" "[^/]*" "\\?"] x}";
   drop = x: substring 0 ((stringLength x) - 1) x;
+  dropFirst = x: substring 1 ((stringLength x) - 1) x;
   lastChar = x: substring ((stringLength x) - 1) 1 x;
   isDir = x: lastChar x == "/";
 
-  gitIgnoreFilter = gitignore: path: type: let
-    baseName = baseNameOf (toString path);
-    relPath = removePrefix (dirOf (toString gitignore) + "/") (toString path);
-    ignoreGlob = filter
-      (nmatch' "^[:space:]*($|#.*)")
-      (splitString "\n" (readFile gitignore));
-    ignoreRegex = map globToRegex ignoreGlob;
-  in (
-    (sources.cleanSourceFilter path type) &&
-    (type != "symlink") &&
-    ! (any (re: let
-      ifDir = optionalString ( (isDir (drop re)) && (type == "directory") ) "/";
-    in
-      ( (match' re (relPath + ifDir)) || (match' re (baseName + ifDir)) )
-    ) ignoreRegex)
-  );
+  getRegex = ignorefile:
+    let
+      ignoreGlob = filter
+        (nmatch' "[\t ]*($|#.*)")
+        (splitString "\n" (readFile ignorefile));
+      ignoreRegex =
+        let res = map globToRegex (filter (nmatch' "!.*") ignoreGlob);
+        in debug "ignore: ${concatStringsSep " " res}" res;
+      unignoreRegex =
+        let res = map (x: globToRegex (dropFirst x)) (filter (match' "!.*") ignoreGlob);
+        in debug "unignore: ${concatStringsSep " " res}" res;
+      rootPath = dirOf (toString ignorefile);
+    in { inherit rootPath ignoreRegex unignoreRegex; };
 
-  gitIgnoreSource = src: let
-    gitignore = (toString src) + "/.gitignore";
-  in
-    filterSource (p: t: let
-      pass = gitIgnoreFilter gitignore p t;
+  gitIgnoreFilter = ignorefile:
+    let
+      inherit (getRegex ignorefile) rootPath ignoreRegex unignoreRegex;
     in
-      debug "${if pass then "o" else "x"} ${p} ${t}" pass
-    ) src;
+      path: type:
+        let
+          relPath = "/" + (removePrefix (rootPath) (toString path));
+          matchFile = re:
+            let
+              isDir' = (type == "directory") && (isDir re);
+              ifDir = optionalString isDir' "/";
+            in (match' re (relPath + ifDir));
+        in (any matchFile unignoreRegex) || (! (any matchFile ignoreRegex));
+
+  gitIgnoreSourceFile = { src, ignorefile ? (toString src) + "/.gitignore" }:
+    cleanSourceWith {
+      filter = gitIgnoreFilter ignorefile; # debugFilter filter' "${baseNameOf ignorefile}";
+      inherit src;
+    };
+
+  gitIgnoreSource = src: gitIgnoreSourceFile { inherit src; };
 in {
-  inherit gitIgnoreSource gitIgnoreFilter;
+  inherit gitIgnoreSource gitIgnoreFilter gitIgnoreSourceFile debugFilter;
 }
